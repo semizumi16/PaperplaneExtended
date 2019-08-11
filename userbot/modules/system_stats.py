0eee54c446b73b577eb6252576dc1f3981952b0f
semizumi <kagura.aileduo@gmail.com>
2019-08-11 08:28:23 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -135,7 +135,7 @@ async def amireallyalive(alive):
     if not alive.text[0].isalpha() and alive.text[0] not in ("/", "#", "@", "!"):
         await alive.edit(
             "`"
-            "My bot is running \n\n"
+            "ShinomiyaKaguya BOT \n\n"
             f"Telethon version: {version.__version__} \n"
             f"Python: {python_version()} \n"
             f"User: {DEFAULTUSER}"
