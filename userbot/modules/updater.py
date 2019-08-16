67ca8be398d96d0cfd367b3718a4b496fce3ef91
semizumi16 <kagura.aileduo@gmail.com>
2019-08-16 05:59:55 +0000

    update update

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -36,7 +36,7 @@ async def upstream(ups):
         "/", "#", "@", "!"):
         await ups.edit("`Checking for updates, please wait....`")
         conf = ups.pattern_match.group(1)
-        off_repo = 'https://github.com/AvinashReddy3108/PaperplaneExtended.git'
+        off_repo = 'https://github.com/semizumi16/PaperplaneExtended.git'
 
         try:
             txt = "`Oops.. Updater cannot continue due to some problems occured`\n\n**LOGTRACE:**\n"
